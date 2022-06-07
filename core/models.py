6d72d08c0a636bcd25f3558d3fa76460f8d3864c
Shabeer Ahammed <mail.shabeerahammed@gmail.com>
2022-06-07 15:29:15 +0530

    Profile model created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=30, null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+
+    @property
+    def full_name(self):
+        # "Returns the person's full name."
+        return '%s %s' % (self.user.first_name, self.user.last_name)
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
