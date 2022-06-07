c7205b38be6752a23714cb11002535e74af8c254
Shabeer Ahammed <mail.shabeerahammed@gmail.com>
2022-06-07 15:51:28 +0530

    Chat Module - Routes and Templates set

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+# from chats.models import Thread
 
-# Create your views here.
+@login_required
+def index(request):
+
+    # threads = Thread.objects.by_user(user=request.user).prefetch_related('chatmessage_thread')
+
+    context = {
+        # 'Threads' : threads
+    }
+
+    return render(request, 'chat.html', context)
