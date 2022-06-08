51f8bf149489f24a0626f6edbd6d89a6040b0f8c
Shabeer Ahammed <mail.shabeerahammed@gmail.com>
2022-06-08 12:51:12 +0530

    Chat - Template setup

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 # from chats.models import Thread
+
+User = get_user_model()
+
 
 @login_required
 def index(request):
 
+    # print("################", request.user.profile.full_name)
+
     # threads = Thread.objects.by_user(user=request.user).prefetch_related('chatmessage_thread')
+    chat_users = User.objects.all().exclude(id=request.user.id)
 
     context = {
         # 'Threads' : threads
+        'ChatUsers': chat_users
     }
 
     return render(request, 'chat.html', context)
