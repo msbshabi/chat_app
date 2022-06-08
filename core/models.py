c6fb80052dbfee04bd3ed19e9214fb6c7d19e480
Shabeer Ahammed <mail.shabeerahammed@gmail.com>
2022-06-08 12:49:50 +0530

    Core model changes and enhancements

--- core/models.py
+++ core/models.py
@@ -5,22 +5,27 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, parent_link=True, primary_key=True)
     bio = models.CharField(max_length=200, null=True, blank=True)
     location = models.CharField(max_length=30, null=True, blank=True)
     birthdate = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return self.user.first_name + " - " + self.user.last_name
+
     @property
     def full_name(self):
-        # "Returns the person's full name."
-        return '%s %s' % (self.user.first_name, self.user.last_name)
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @property
+    def avatar_url(self):
+        return f'https://avatars.dicebear.com/api/avataaars/{self.full_name}.png?style=circle&eyes[]=default&mouth[]=default&skin[]=light&hairColor[]=black'
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
