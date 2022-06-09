44a6752365acbe1bf4808614f7c7e9ae1046b99e
Shabeer Ahammed <mail.shabeerahammed@gmail.com>
2022-06-09 10:42:17 +0530

    UUID Implementation on models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,39 @@
+import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 
-# Create your models here.
+User = get_user_model()
+
+
+class ThreadManager(models.Manager):
+    def by_user(self, **kwargs):
+        user = kwargs.get('user')
+        lookup = Q(first_person=user) | Q(second_person=user)
+        qs = self.get_queryset().filter(lookup).distinct()
+        return qs
+
+
+class Thread(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_person = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, related_name='thread_first_person')
+    second_person = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True, related_name='thread_second_person')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = ThreadManager()
+
+    class Meta:
+        unique_together = ['first_person', 'second_person']
+
+
+class ChatMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    thread = models.ForeignKey(
+        Thread, null=True, blank=True, on_delete=models.CASCADE, related_name='chatmessage_thread')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    read_at = models.DateTimeField(null=True, blank=True)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -5,7 +6,8 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, parent_link=True, primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, parent_link=True)
     bio = models.CharField(max_length=200, null=True, blank=True)
     location = models.CharField(max_length=30, null=True, blank=True)
     birthdate = models.DateField(null=True, blank=True)
@@ -26,6 +28,7 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
